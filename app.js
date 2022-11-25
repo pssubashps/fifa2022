b57110556c97a015a0d0349c38e6dd8b9a3684e5
Subash PS <pssubashps@gmail.com>
2022-11-25 11:45:04 +0530

    Update app.js

--- app.js
+++ app.js
@@ -123,3 +123,15 @@ Vue.createApp({
         }
     }
 }).mount('#app');
+
+
+let beforeInstallPrompt = null;
+window.addEventListener("beforeinstallprompt", eventHandler, errorHandler);
+
+function eventHandler(event){
+    console.log('event triggered');
+    beforeInstallPrompt = event;        
+}
+function errorHandler(e){
+    console.log('error: ' + e);
+}
