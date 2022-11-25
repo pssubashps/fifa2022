8908ee29496120363fb30a8b0f22a5cf7cf7317c
Subash PS <pssubashps@gmail.com>
2022-11-25 12:44:56 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -9,3 +9,16 @@ function eventHandler(event){
 function errorHandler(e){
     console.log('error: ' + e);
 }
+self.addEventListener('fetch', function(event) {
+    // either respond with the cached object or go ahead and fetch the actual url
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            if (response) {
+                // retrieve from cache
+                return response;
+            }
+            // fetch as normal
+            return fetch(event.request);
+        })
+    );
+});
